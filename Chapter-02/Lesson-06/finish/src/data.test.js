98d77414c68aeb4f996c5bfb9dca2643fd22a252
Carl Rippon <carl.rippon@googlemail.com>
2021-02-07 10:40:52 +0000

    lesson 6 finish

---
+++ Chapter-02/Lesson-06/finish/src/data.test.js
@@ -0,0 +1,16 @@
+import { getName } from "./data";
+
+test("getName should return correct name when 0 passed in", async () => {
+  const name = await getName(0);
+  expect(name).toBe("Bill");
+});
+
+test("getName should return correct name when 1 passed in", async () => {
+  const name = await getName(1);
+  expect(name).toBe("Jane");
+});
+
+test("getName should return null when 2 passed", async () => {
+  const name = await getName(2);
+  expect(name).toBeNull();
+});
