640c35329dad55597d65e82497123b309cd4005c
Carl Rippon <carl.rippon@googlemail.com>
2021-02-09 04:15:08 +0000

    lesson 3 update

--- Chapter-03/Lesson-03/finish/src/data.getPerson.test.js
+++ Chapter-03/Lesson-03/finish/src/data.getPerson.test.js
@@ -2,9 +2,10 @@ import { getPerson } from "./data";
 
 test("Should return correct person object when found", async () => {
   const person = await getPerson(1);
-  expect(person).toStrictEqual({
-    id: 1,
-    firstName: "Bill",
-    lastName: "Peters",
-  });
+  expect(person).toStrictEqual(
+    expect.objectContaining({
+      firstName: "Bill",
+      lastName: "Peters",
+    })
+  );
 });
