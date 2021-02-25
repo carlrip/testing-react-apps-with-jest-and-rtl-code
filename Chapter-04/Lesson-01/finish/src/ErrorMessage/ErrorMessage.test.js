53360e39aecf17281b608659f378e582a5c03f40
Carl Rippon <carl.rippon@googlemail.com>
2021-02-25 19:32:25 +0000

    ch4 lesson 1

---
+++ Chapter-04/Lesson-01/finish/src/ErrorMessage/ErrorMessage.test.js
@@ -0,0 +1,16 @@
+import { render } from "react-dom";
+import { act } from "react-dom/test-utils";
+import { ErrorMessage } from "./ErrorMessage";
+
+test("Should render correct message when message prop passed", () => {
+  const container = document.createElement("div");
+  document.body.appendChild(container);
+
+  act(() => {
+    render(<ErrorMessage message="test" />, container);
+  });
+
+  const div = document.querySelector(".saveerror");
+
+  expect(div.textContent).toBe("test");
+});
