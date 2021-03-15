56bac70a8aff429342d80cf320f733489078b57e
Carl Rippon <carl.rippon@googlemail.com>
2021-03-15 05:07:41 +0000

    tweeks

--- Chapter-06/Lesson-01/start/src/ContactForm/ContactForm.test.js
+++ Chapter-06/Lesson-01/start/src/ContactForm/ContactForm.test.js
@@ -1,9 +1,9 @@
-import { render, screen, fireEvent } from "@testing-library/react";
+import { render, screen } from "@testing-library/react";
 import { ContactForm } from "./ContactForm";
 
 test("Clicking submit button without filling in form renders name validation error", async () => {
   render(<ContactForm />);
-  fireEvent(screen.getByText("Save"), new MouseEvent("click"));
+
   expect(
     await screen.findByText("You must enter your name")
   ).toBeInTheDocument();
