a7fe3c4b2efcd8f298156ff3f9c0f2cebc446205
Carl Rippon <carl.rippon@googlemail.com>
2021-03-25 19:03:22 +0000

    exercise finish

---
+++ Chapter-06/Lesson-07/finish/src/SearchForm/SearchForm.js
@@ -0,0 +1,47 @@
+import React from "react";
+import { useForm } from "react-hook-form";
+import { search } from "./data";
+import "./SearchForm.css";
+
+export function SearchForm() {
+  const { register, errors, handleSubmit, reset, formState } = useForm();
+  const [results, setResults] = React.useState([]);
+
+  async function onSubmit({ criteria }) {
+    setResults(await search(criteria));
+  }
+
+  function handleClear() {
+    setResults([]);
+    reset();
+  }
+
+  return (
+    <form onSubmit={handleSubmit(onSubmit)} noValidate className="form">
+      <div className="row">
+        <input
+          type="search"
+          name="criteria"
+          placeholder="Enter search criteria"
+          ref={register({ required: true })}
+        />
+        <button type="submit">Search</button>
+      </div>
+      {errors.criteria && errors.criteria.type === "required" && (
+        <div className="field-error">You must enter some search criteria</div>
+      )}
+      {formState.isSubmitting && <p>Searching ...</p>}
+      <div className="row">
+        {results.map((result) => (
+          <div key={result}>{result}</div>
+        ))}
+      </div>
+      {results.length === 0 && formState.isSubmitSuccessful && (
+        <p>None found</p>
+      )}
+      {results.length > 0 && formState.isSubmitSuccessful && (
+        <button onClick={handleClear}>Clear</button>
+      )}
+    </form>
+  );
+}
---
+++ Chapter-06/Lesson-07/finish/src/SearchForm/SearchForm.test.js
@@ -0,0 +1,49 @@
+import { render, screen } from "@testing-library/react";
+import { SearchForm } from "./SearchForm";
+import userEvent from "@testing-library/user-event";
+
+test("Submitting a search with no criteria should render validation error", async () => {
+  render(<SearchForm />);
+  userEvent.click(screen.getByText("Search"));
+  expect(
+    await screen.findByText("You must enter some search criteria")
+  ).toBeInTheDocument();
+});
+
+test("Submitting a search with criteria should render correct results", async () => {
+  render(<SearchForm />);
+  userEvent.type(screen.getByPlaceholderText("Enter search criteria"), "app");
+  userEvent.click(screen.getByText("Search"));
+  expect(await screen.findByText("Apple")).toBeInTheDocument();
+});
+
+test("Search indicator should show during search request", async () => {
+  render(<SearchForm />);
+  userEvent.type(screen.getByPlaceholderText("Enter search criteria"), "app");
+  userEvent.click(screen.getByText("Search"));
+  expect(screen.getByText("Searching ...")).toBeInTheDocument();
+  await screen.findByText("Apple");
+  expect(screen.queryByText("Searching ...")).not.toBeInTheDocument();
+});
+
+test("Submitting a search with criteria that does not match should render not found message", async () => {
+  render(<SearchForm />);
+  userEvent.type(
+    screen.getByPlaceholderText("Enter search criteria"),
+    "unknown"
+  );
+  userEvent.click(screen.getByText("Search"));
+  expect(await screen.findByText("None found")).toBeInTheDocument();
+});
+
+test("Clicking Clear button should clear criteria and results", async () => {
+  render(<SearchForm />);
+  userEvent.type(screen.getByPlaceholderText("Enter search criteria"), "app");
+  userEvent.click(screen.getByText("Search"));
+  await screen.findByText("Apple");
+  userEvent.click(screen.getByText("Clear"));
+  expect(
+    screen.getByPlaceholderText("Enter search criteria")
+  ).toHaveDisplayValue("");
+  expect(screen.queryByText("Apple")).not.toBeInTheDocument();
+});
