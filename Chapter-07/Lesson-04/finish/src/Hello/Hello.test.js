4e4f2af0c2ff1ba947fd0a87870badf0054eda8e
Carl Rippon <carl.rippon@googlemail.com>
2021-04-08 18:59:34 +0100

    lesson 4 finish

---
+++ Chapter-07/Lesson-04/finish/src/Hello/Hello.test.js
@@ -0,0 +1,25 @@
+import { render, screen } from "@testing-library/react";
+import { Hello } from "./Hello";
+import * as data from "./data";
+
+test("Should include users name when rendered", async () => {
+  data.getUser = jest
+    .spyOn(data, "getUser")
+    .mockResolvedValue({ id: 1, name: "Bob" });
+
+  render(<Hello id={1} />);
+
+  expect(await screen.findByText(/Bob/)).toBeInTheDocument();
+  expect(data.getUser).toHaveBeenCalled();
+  expect(data.getUser).toHaveBeenCalledTimes(1);
+  expect(data.getUser).toHaveBeenCalledWith(1);
+
+  data.getUser.mockClear();
+  await data.getUser(3);
+  expect(data.getUser).toHaveBeenCalledTimes(1);
+
+  data.getUser.mockReset();
+  console.log(await data.getUser(3));
+
+  data.getUser.mockRestore();
+});
