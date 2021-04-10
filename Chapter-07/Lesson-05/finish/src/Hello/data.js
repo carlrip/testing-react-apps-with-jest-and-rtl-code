91c68cdd188aa39bef06b594c5787700ca4f807c
Carl Rippon <carl.rippon@googlemail.com>
2021-04-10 11:51:52 +0100

    lesson 5

---
+++ Chapter-07/Lesson-05/finish/src/Hello/data.js
@@ -0,0 +1,11 @@
+export async function getUser(id) {
+  const response = await fetch(`https://some-server/users/${id}`);
+  const data = await response.json();
+  return data;
+}
+
+export async function getUserRole(id) {
+  const response = await fetch(`https://some-server/role/${id}`);
+  const data = await response.json();
+  return data;
+}
--- Chapter-07/Lesson-05/start/src/Hello/Hello.test.js
+++ Chapter-07/Lesson-05/start/src/Hello/Hello.test.js
@@ -2,11 +2,42 @@ import { render, screen } from "@testing-library/react";
 import { Hello } from "./Hello";
 import * as data from "./data";
 
+jest.mock("./data");
+
+beforeEach(() => {
+  data.getUser.mockResolvedValue({
+    id: 0,
+    name: "",
+  });
+});
+afterEach(() => {
+  jest.resetAllMocks();
+});
+
 test("Should include users name when rendered", async () => {
+  data.getUser.mockResolvedValue({
+    id: 1,
+    name: "Bob",
+  });
+
   render(<Hello id={1} />);
 
   expect(await screen.findByText(/Bob/)).toBeInTheDocument();
   expect(data.getUser).toHaveBeenCalled();
   expect(data.getUser).toHaveBeenCalledTimes(1);
   expect(data.getUser).toHaveBeenCalledWith(1);
+});
+
+test("Should include users role when rendered", async () => {
+  console.log(data.getUser(1));
+  data.getUserRole.mockResolvedValue({
+    role: "Administrator",
+  });
+
+  render(<Hello id={1} />);
+
+  expect(await screen.findByText(/Administrator/)).toBeInTheDocument();
+  expect(data.getUserRole).toHaveBeenCalled();
+  expect(data.getUserRole).toHaveBeenCalledTimes(1);
+  expect(data.getUserRole).toHaveBeenCalledWith(1);
 });
