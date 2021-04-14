3b434df0398595cc386b84530b2b06cfbe0fa851
Carl Rippon <carl.rippon@googlemail.com>
2021-04-14 19:14:44 +0100

    start fix

--- Chapter-07/Lesson-08/start/src/PersonForm/PersonForm.test.js
+++ Chapter-07/Lesson-08/start/src/PersonForm/PersonForm.test.js
@@ -11,5 +11,9 @@ test("Should render success message when form successfully submitted", async () 
 });
 
 test("Should render error message when invalid form submitted", async () => {
-  // TODO
+  // TODO - mock fetch
+  render(<PersonForm />);
+  // TODO - submit the form with a name of Fred and a score of 10
+  expect(await screen.findByText(/problem/i)).toBeInTheDocument();
+  // TODO - restore fetch
 });
