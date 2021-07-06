8d3505c7bd6acbcaf971323277a95411370577fb
Carl Rippon <carl.rippon@googlemail.com>
2021-07-06 20:39:44 +0100

    fix

--- Chapter-08/Lesson-06/finish/src/Data/useGetNotes.test.js
+++ Chapter-08/Lesson-06/finish/src/Data/useGetNotes.test.js
@@ -2,32 +2,61 @@ import { waitFor } from "@testing-library/react";
 import { renderHook } from "@testing-library/react-hooks";
 import { useGetNotes } from "./useGetNotes";
 
-test("Should return loading true initially", () => {
-    const { result } = renderHook(() => useGetNotes());
-    expect(result.current.notesLoading).toBe(true);
+test("Should return loading true initially", async () => {
+  jest.spyOn(window, "fetch");
+  fetch.mockResolvedValue(
+    new Response(
+      JSON.stringify([
+        {
+          id: 1,
+          note: "This is an important note.",
+        },
+      ]),
+      { status: 200 }
+    )
+  );
+
+  const { result } = renderHook(() => useGetNotes());
+  expect(result.current.notesLoading).toBe(true);
+
+  await waitFor(() => {
+    expect(result.current.notes).toStrictEqual([
+      {
+        id: 1,
+        note: "This is an important note.",
+      },
+    ]);
+  });
+
+  fetch.mockRestore();
 });
 
 test("Should return notes and loading false after loaded", async () => {
-    jest.spyOn(window, "fetch");
-    fetch.mockResolvedValue(
-        new Response(JSON.stringify([
+  jest.spyOn(window, "fetch");
+  fetch.mockResolvedValue(
+    new Response(
+      JSON.stringify([
         {
-            "id": 1,
-            "note": "This is an important note."
-        }
-        ]), { status: 200 })
-    );
+          id: 1,
+          note: "This is an important note.",
+        },
+      ]),
+      { status: 200 }
+    )
+  );
 
-    const { result } = renderHook(() => useGetNotes());
+  const { result } = renderHook(() => useGetNotes());
 
-    await waitFor(() => {
-        expect(result.current.notes).toStrictEqual([{
-            "id": 1,
-            "note": "This is an important note."
-        }]);
-    });
+  await waitFor(() => {
+    expect(result.current.notes).toStrictEqual([
+      {
+        id: 1,
+        note: "This is an important note.",
+      },
+    ]);
+  });
 
-    expect(result.current.notesLoading).toBe(false);
+  expect(result.current.notesLoading).toBe(false);
 
-    fetch.mockRestore();
+  fetch.mockRestore();
 });
