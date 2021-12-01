7491c1ac807c3aaaa6e83f7e6d3f303ee08464de
carl <carl.rippon@theaccessgroup.com>
2021-12-01 04:38:02 +0000

    Remove redundant act

--- Chapter-06/Lesson-07/finish/src/SearchForm/SearchForm.test.js
+++ Chapter-06/Lesson-07/finish/src/SearchForm/SearchForm.test.js
@@ -1,4 +1,4 @@
-import { render, screen, act } from "@testing-library/react";
+import { render, screen } from "@testing-library/react";
 import { SearchForm } from "./SearchForm";
 import userEvent from "@testing-library/user-event";
 
