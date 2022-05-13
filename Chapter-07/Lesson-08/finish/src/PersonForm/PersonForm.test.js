9b2ca8d9bef6a5b079f88ddbed1872e2652c61de
Carl Rippon <carl.rippon@theaccessgroup.com>
2022-05-13 20:25:22 +0100

    updated @testing-library/user-event

--- Chapter-07/Lesson-08/finish/src/PersonForm/PersonForm.test.js
+++ Chapter-07/Lesson-08/finish/src/PersonForm/PersonForm.test.js
@@ -3,6 +3,8 @@ import { PersonForm } from "./PersonForm";
 import userEvent from "@testing-library/user-event";
 
 test("Should render success message when form successfully submitted", async () => {
+  const user = userEvent.setup();
+
   jest.spyOn(window, "fetch");
   fetch.mockResolvedValue(
     new Response(JSON.stringify({ id: 1 }), { status: 200 })
@@ -10,9 +12,13 @@ test("Should render success message when form successfully submitted", async () 
 
   render(<PersonForm />);
 
-  userEvent.type(screen.getByLabelText("Name"), "Fred");
-  userEvent.type(screen.getByLabelText("Score"), "10");
-  userEvent.click(screen.getByText("Save"));
+  screen.getByLabelText("Name").focus();
+  await user.keyboard("Fred");
+
+  screen.getByLabelText("Score").focus();
+  await user.keyboard("10");
+
+  await user.click(screen.getByText("Save"));
 
   expect(await screen.findByText(/successfully added/i)).toBeInTheDocument();
 
@@ -20,6 +26,7 @@ test("Should render success message when form successfully submitted", async () 
 });
 
 test("Should render error message when invalid form submitted", async () => {
+  const user = userEvent.setup();
   jest.spyOn(window, "fetch");
   fetch.mockResolvedValue(
     new Response(JSON.stringify({ errors: { Name: "Invalid name" } }), {
@@ -29,9 +36,13 @@ test("Should render error message when invalid form submitted", async () => {
 
   render(<PersonForm />);
 
-  userEvent.type(screen.getByLabelText("Name"), "Fred");
-  userEvent.type(screen.getByLabelText("Score"), "10");
-  userEvent.click(screen.getByText("Save"));
+  screen.getByLabelText("Name").focus();
+  await user.keyboard("Fred");
+
+  screen.getByLabelText("Score").focus();
+  await user.keyboard("10");
+
+  await user.click(screen.getByText("Save"));
 
   expect(await screen.findByText(/problem/i)).toBeInTheDocument();
 
