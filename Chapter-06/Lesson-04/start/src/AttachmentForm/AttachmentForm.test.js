7654766d57d890e5f9e5286c581efbbdcda45020
Carl Rippon <carl.rippon@theaccessgroup.com>
2022-05-13 19:36:40 +0100

    updated @testing-library/user-event

--- Chapter-06/Lesson-04/start/src/AttachmentForm/AttachmentForm.test.js
+++ Chapter-06/Lesson-04/start/src/AttachmentForm/AttachmentForm.test.js
@@ -3,7 +3,8 @@ import { AttachmentForm } from "./AttachmentForm";
 import userEvent from "@testing-library/user-event";
 
 test("Clicking Upload button when file has been selected should render a success message containing file name", async () => {
+  const user = userEvent.setup();
   render(<AttachmentForm />);
 
-  userEvent.click(screen.getByText("Upload"));
+  await user.click(screen.getByText("Upload"));
 });
