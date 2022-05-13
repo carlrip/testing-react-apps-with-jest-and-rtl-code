eabdb58a8c27f0faedeb5d16b8d5c9016394cf9d
Carl Rippon <carl.rippon@theaccessgroup.com>
2022-05-13 19:44:35 +0100

    updated @testing-library/user-event

--- Chapter-06/Lesson-04/finish/src/AttachmentForm/AttachmentForm.test.js
+++ Chapter-06/Lesson-04/finish/src/AttachmentForm/AttachmentForm.test.js
@@ -3,23 +3,27 @@ import { AttachmentForm } from "./AttachmentForm";
 import userEvent from "@testing-library/user-event";
 
 test("Clicking Upload button when file has been selected should render a success message containing file name", async () => {
+  const user = userEvent.setup();
+
   render(<AttachmentForm />);
   const file = new File(["test"], "test.txt", { type: "plain/text" });
-  userEvent.upload(screen.getByLabelText("Attachment"), file);
-  userEvent.click(screen.getByText("Upload"));
+  await user.upload(screen.getByLabelText("Attachment"), file);
+  await user.click(screen.getByText("Upload"));
   expect(
     await screen.findByText("Successfully uploaded test.txt")
   ).toBeInTheDocument();
 });
 
 test("Clicking Upload button when two files have been selected should render a success message containing the file names", async () => {
+  const user = userEvent.setup();
+
   render(<AttachmentForm />);
   const files = [
     new File(["test1"], "test1.txt", { type: "plain/text" }),
     new File(["test2"], "test2.txt", { type: "plain/text" }),
   ];
-  userEvent.upload(screen.getByLabelText("Attachment"), files);
-  userEvent.click(screen.getByText("Upload"));
+  await user.upload(screen.getByLabelText("Attachment"), files);
+  await user.click(screen.getByText("Upload"));
   expect(
     await screen.findByText("Successfully uploaded test1.txt, test2.txt")
   ).toBeInTheDocument();
