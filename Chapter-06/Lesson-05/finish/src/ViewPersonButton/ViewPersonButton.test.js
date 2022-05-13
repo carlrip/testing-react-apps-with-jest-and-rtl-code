01b85084c96cbbe32d85be0e3a766019742932e9
Carl Rippon <carl.rippon@theaccessgroup.com>
2022-05-13 20:09:35 +0100

    updated @testing-library/user-event

--- Chapter-06/Lesson-05/finish/src/ViewPersonButton/ViewPersonButton.test.js
+++ Chapter-06/Lesson-05/finish/src/ViewPersonButton/ViewPersonButton.test.js
@@ -3,12 +3,14 @@ import { ViewPersonButton } from "./ViewPersonButton";
 import userEvent from "@testing-library/user-event";
 
 test("Hovering over button shows persons name", async () => {
+  const user = userEvent.setup();
   render(<ViewPersonButton personId={1} />);
 
-  userEvent.hover(screen.getByText("View"));
+  await user.hover(screen.getByText("View"));
 
   expect(await screen.findByText("Bill Peters")).toBeInTheDocument();
 
-  userEvent.unhover(screen.getByText("View"));
+  await user.unhover(screen.getByText("View"));
+  
   expect(screen.queryByText("Bill Peters")).not.toBeInTheDocument();
 });
