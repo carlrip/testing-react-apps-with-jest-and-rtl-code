d2ae9f4fa2257450562a8969e3b6f13a098ee307
Carl Rippon <carl.rippon@theaccessgroup.com>
2022-05-13 18:36:02 +0100

    updated @testing-library/user-event

--- Chapter-06/Lesson-01/finish/src/ContactForm/ContactForm.test.js
+++ Chapter-06/Lesson-01/finish/src/ContactForm/ContactForm.test.js
@@ -3,10 +3,11 @@ import { ContactForm } from "./ContactForm";
 import userEvent from "@testing-library/user-event";
 
 test("Clicking submit button without filling in form renders name validation error", async () => {
+  const user = userEvent.setup();
   render(<ContactForm />);
   const saveButton = screen.getByText("Save");
   // fireEvent(saveButton, new MouseEvent("click"));
-  userEvent.click(saveButton);
+  user.click(saveButton);
   expect(
     await screen.findByText("You must enter your name")
   ).toBeInTheDocument();
