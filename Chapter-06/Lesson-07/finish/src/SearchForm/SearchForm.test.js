aacfce19b87fdb63586036ff431e213eea020106
Carl Rippon <carl.rippon@theaccessgroup.com>
2022-05-13 20:20:52 +0100

    updated @testing-library/user-event

--- Chapter-06/Lesson-07/finish/src/SearchForm/SearchForm.test.js
+++ Chapter-06/Lesson-07/finish/src/SearchForm/SearchForm.test.js
@@ -3,45 +3,71 @@ import { SearchForm } from "./SearchForm";
 import userEvent from "@testing-library/user-event";
 
 test("Submitting a search with no criteria should render validation error", async () => {
+  const user = userEvent.setup();
+
   render(<SearchForm />);
-  userEvent.click(screen.getByText("Search"));
+  await user.click(screen.getByText("Search"));
   expect(
     await screen.findByText("You must enter some search criteria")
   ).toBeInTheDocument();
 });
 
 test("Submitting a search with criteria should render correct results", async () => {
+  const user = userEvent.setup();
+
   render(<SearchForm />);
-  userEvent.type(screen.getByPlaceholderText("Enter search criteria"), "app");
-  userEvent.click(screen.getByText("Search"));
+
+  screen.getByPlaceholderText("Enter search criteria").focus();
+  await user.keyboard("app");
+
+  await user.click(screen.getByText("Search"));
+  
   expect(await screen.findByText("Apple")).toBeInTheDocument();
 });
 
 test("Search indicator should show during search request", async () => {
+  const user = userEvent.setup();
+
   render(<SearchForm />);
-  userEvent.type(screen.getByPlaceholderText("Enter search criteria"), "app");
-  userEvent.click(screen.getByText("Search"));
+
+  screen.getByPlaceholderText("Enter search criteria").focus();
+  await user.keyboard("app");
+
+  await user.click(screen.getByText("Search"));
+  
   expect(screen.getByText("Searching ...")).toBeInTheDocument();
   await screen.findByText("Apple");
   expect(screen.queryByText("Searching ...")).not.toBeInTheDocument();
 });
 
 test("Submitting a search with criteria that does not match should render not found message", async () => {
+  const user = userEvent.setup();
+
   render(<SearchForm />);
-  userEvent.type(
-    screen.getByPlaceholderText("Enter search criteria"),
-    "unknown"
-  );
-  userEvent.click(screen.getByText("Search"));
+
+  screen.getByPlaceholderText("Enter search criteria").focus();
+  await user.keyboard("unknown");
+
+  await user.click(screen.getByText("Search"));
+  
   expect(await screen.findByText("None found")).toBeInTheDocument();
 });
 
 test("Clicking Clear button should clear criteria and results", async () => {
+  const user = userEvent.setup();
+
   render(<SearchForm />);
-  userEvent.type(screen.getByPlaceholderText("Enter search criteria"), "app");
-  userEvent.click(screen.getByText("Search"));
+
+  screen.getByPlaceholderText("Enter search criteria").focus();
+
+  await user.keyboard("app");
+
+  await user.click(screen.getByText("Search"));
+  
   await screen.findByText("Apple");
-  userEvent.click(screen.getByText("Clear"));
+  
+  await user.click(screen.getByText("Clear"));
+  
   expect(
     await screen.findByPlaceholderText("Enter search criteria")
   ).toHaveDisplayValue("");
