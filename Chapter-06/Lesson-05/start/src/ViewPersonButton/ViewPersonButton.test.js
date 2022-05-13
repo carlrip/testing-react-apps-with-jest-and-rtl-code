1841128a547230651381857996755b41820aa0e0
Carl Rippon <carl.rippon@theaccessgroup.com>
2022-05-13 19:46:59 +0100

    updated @testing-library/user-event

--- Chapter-06/Lesson-05/start/src/ViewPersonButton/ViewPersonButton.test.js
+++ Chapter-06/Lesson-05/start/src/ViewPersonButton/ViewPersonButton.test.js
@@ -3,5 +3,7 @@ import { ViewPersonButton } from "./ViewPersonButton";
 import userEvent from "@testing-library/user-event";
 
 test("Hovering over button shows persons name", async () => {
+  const user = userEvent.setup();
+  
   render(<ViewPersonButton personId={1} />);
 });
